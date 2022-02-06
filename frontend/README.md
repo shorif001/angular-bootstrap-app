c22b38340d6b693fb97b23297fe03edb41cae554
Shorif <sashipon001@gmail.com>
2022-02-06 22:46:32 +0600

    angular bootstrap app

---
+++ frontend/README.md
@@ -0,0 +1,30 @@
+# Angular 13 / Bootstrap 5 & CRUD REST API
+# Frontend
+
+
+### Installation
+* `npm install` (installing dependencies)
+
+### Developpement
+* `npm run start`
+* in your browser [http://localhost:4200](http://localhost:4200) 
+
+## Tests
+* `npm run test`
+
+### Compilation
+* `npm run build`       ( without SSR)
+* `npm run build:ssr`   ( with SSR)
+
+### Production
+* `npm run serve:ssr`
+
+* in your browser [http://localhost:4000](http://localhost:4000) 
+
+
+## Documentation
+English Tutorials
+- Step by Step - http://www.ganatan.com/tutorials/en
+
+Tutoriels en français
+- Etapes par étapes - http://www.ganatan.com/tutorials
---
+++ frontend/src/app/modules/general/home/home.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Meta, Title } from '@angular/platform-browser';
+
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  name = environment.application.name;
+  angular = environment.application.angular;
+  bootstrap = environment.application.bootstrap;
+  fontawesome = environment.application.fontawesome;
+
+  features: any;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    private meta: Meta,
+    private titleService: Title) {
+  }
+
+  ngOnInit(): void {
+
+    if (isPlatformBrowser(this.platformId)) {
+      let navMain = document.getElementById('navbarCollapse');
+      if (navMain) {
+        navMain.onclick = function () {
+          if (navMain) {
+            navMain.classList.remove("show");
+          }
+        }
+      }
+    }
+
+    this.titleService.setTitle('angular.ganatan.com : An Angular Application Web');
+    this.meta.addTag({
+      name: 'author',
+      content: 'danny ganatan'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'This application was developed with Angular version 13.2.1 and bootstrap 5.1.3' +
+          ' It applies Routing, Lazy loading, Server side rendering and Progressive Web App (PWA)'
+      });
+  }
+
+  loadScript(name: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.src = name;
+      s.async = false;
+      document.getElementsByTagName('head')[0].appendChild(s);
+    }
+  }
+
+}
+
